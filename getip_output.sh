7e6738a663e5460d517f7fa49d58dfffbac5aa8a
nayuta1999 <youmu331@gmail.com>
2024-05-19 22:37:40 +0900

    httpd2.4への対応

--- getip_output.sh
+++ getip_output.sh
@@ -7,10 +7,12 @@ IPList=($IPList)
 date=$(date +%Y-%m-%d)
 
 
-httpd_allow=teikei
-httpd_deny=teikei
-nginx_allow=teikei
-nginx_deny=teikei
+httpd_allow=$teikei
+httpd_deny=$teikei
+nginx_allow=$teikei
+nginx_deny=$teikei
+httpd24_allow=$teikei
+httpd24_deny=$teikei
 #1つしか表示されてないので直す
 for ip in ${IPList[@]}
 do
@@ -18,10 +20,13 @@ do
 	httpd_deny=$httpd_deny"deny from ${ip} \n"
 	nginx_allow=$nginx_allow"allow ${ip}; \n"
 	nginx_deny=$nginx_deny"deny ${ip}; \n"
+	httpd24_allow=$httpd24_allow"Require ip ${ip}\n"
+	httpd24_deny=$httpd24_deny"Require not ip ${ip}\n"
 done
 
 echo -e $httpd_allow > $DocumentRoot/jp/httpd/allow.txt
 echo -e $httpd_deny > $DocumentRoot/jp/httpd/deny.txt
 echo -e $nginx_allow > $DocumentRoot/jp/nginx/allow.txt
 echo -e $nginx_deny > $DocumentRoot/jp/nginx/deny.txt
-
+echo -e $httpd24_allow > $DocumentRoot/jp/httpd24/allow.txt
+echo -e $httpd24_deny > $DocumentRoot/jp/httpd24/deny.txt
